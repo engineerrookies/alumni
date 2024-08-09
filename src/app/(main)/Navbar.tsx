1fc96952d0296eed2d3ca889500d047b0e4f95a7
engineerrookies <cgriffiths@csumb.edu>
2024-08-08 19:10:50 -0700

    Navbar back to og

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -7,11 +7,18 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <header className="sticky top-0 z-10 bg-card shadow-sm">
-      <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-4 px-5 py-3">
+      <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-5 px-5 py-3">
         <Link href="/" className="flex items-center text-x1 font-bold text-primary">
-          SDD Alumni!
+          <Image
+            src={logoImage}
+            alt="SDD Alumni Logo"
+            width={35}
+            height={35}
+            className="mr-2"
+          />
+          Alumni!
         </Link>
-        <SearchField />
+          <SearchField />
         <UserButton className="sm:ms-auto" />
       </div>
     </header>
